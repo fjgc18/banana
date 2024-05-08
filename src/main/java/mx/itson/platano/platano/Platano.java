4e8b6d78d64c6bb7fe693f24ce18da70ece4a717
Thinkpad <Thinkpad@10.232.15.216>
2024-05-08 11:48:40 -0600

    Primera version

---
+++ src/main/java/mx/itson/platano/platano/Platano.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.platano.platano;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class Platano {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
